26f02f28495146a23a7838608cc9b0ceb4e4795a
AntonioE <antonio.esposito4@live.it>
2021-04-05 18:53:35 +0200

    fix strongly typed user

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -4,7 +4,7 @@ import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 
 import { AuthService } from './auth.service';
-import { getMaskUsername } from './state/user.reducer';
+import { getMaskUsername, State } from './state/user.reducer';
 
 @Component({
   templateUrl: './login.component.html',
@@ -18,7 +18,7 @@ export class LoginComponent implements OnInit {
   constructor(
     private authService: AuthService,
     private router: Router,
-    private store: Store<any>
+    private store: Store<State>
   ) {}
 
   ngOnInit(): void {
