6aa23d23f2e860be6179822696976654553ab3ed
AntonioE <antonio.esposito4@live.it>
2021-04-05 18:34:00 +0200

    strongly typed product state

--- src/app/products/state/product.reducer.ts
+++ src/app/products/state/product.reducer.ts
@@ -1,11 +1,59 @@
-import { createAction, createReducer, on } from '@ngrx/store';
+import {
+  createAction,
+  createFeatureSelector,
+  createReducer,
+  createSelector,
+  on,
+} from '@ngrx/store';
+import { Product } from '../product';
+import * as AppState from '../../state/app.state';
 
-export const productReducer = createReducer(
-  { showProductCode: true },
-  on(createAction('[Product] Toggle Product Code'), (state) => {
-    return {
-      ...state,
-      showProductCode: !state.showProductCode,
-    };
-  })
+// State
+export interface State extends AppState.State {
+  products: ProductState;
+}
+
+export interface ProductState {
+  showProductCode: boolean;
+  currentProduct: Product;
+  products: Product[];
+}
+
+// Initial State
+const initialState: ProductState = {
+  showProductCode: true,
+  currentProduct: null,
+  products: [],
+};
+
+// Selector
+const getProductFeatureState = createFeatureSelector<ProductState>('products');
+
+export const getShowProductCode = createSelector(
+  getProductFeatureState,
+  (state) => state.showProductCode
+);
+
+export const getCurrentProduct = createSelector(
+  getProductFeatureState,
+  (state) => state.currentProduct
+);
+
+export const getProducts = createSelector(
+  getProductFeatureState,
+  (state) => state.products
+);
+
+// Reducer
+export const productReducer = createReducer<ProductState>(
+  initialState,
+  on(
+    createAction('[Product] Toggle Product Code'),
+    (state): ProductState => {
+      return {
+        ...state,
+        showProductCode: !state.showProductCode,
+      };
+    }
+  )
 );
